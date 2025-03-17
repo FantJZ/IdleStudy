64c55f71cc79f18832ef9241292706fded8e094c
FantJZ <jz.2002@icloud.com>
2025-03-17 20:45:57 +0000

    2025.03.17 0.1 build 2
    移除Start按钮后的弹窗
    将滑块添加到首页

---
+++ IdleStudy/FishingPage/SlideBar.swift
@@ -0,0 +1,18 @@
+//
+//  SlideBar.swift
+//  IdleStudy
+//
+//  Created by JZ on 2025/3/17.
+//
+
+import SwiftUI
+
+struct SlideBar: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SlideBar()
+}

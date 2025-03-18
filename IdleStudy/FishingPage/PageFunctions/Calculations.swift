3e206a584d39f2903dd9d2e8ba3424f461928e11
FantJZ <jz.2002@icloud.com>
2025-03-18 00:24:43 +0000

    2025.03.18 0.1 build 3
    完善了重量和价格的计算，尚未将其加入UI

---
+++ IdleStudy/FishingPage/PageFunctions/Calculations.swift
@@ -0,0 +1,8 @@
+//
+//  Calculations.swift
+//  IdleStudy
+//
+//  Created by JZ on 2025/3/17.
+//
+
+import Foundation
---
+++ IdleStudy/Functions.swift
@@ -0,0 +1,18 @@
+//
+//  Functions.swift
+//  IdleStudy
+//
+//  Created by JZ on 2025/3/17.
+//
+
+import SwiftUI
+
+struct Functions: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Functions()
+}
